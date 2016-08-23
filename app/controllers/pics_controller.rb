e5f1c85d8f90031e9f14af1046c2db4181454a02
IuliiaKot <kotlenko.julia@gmail.com>
2016-08-22 21:15:57 -0400

    add devise and association

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -11,12 +11,12 @@ class PicsController < ApplicationController
   end
 
   def new
-    @pic = Pic.new
+    @pic = current_user.photos.build
   end
 
 
   def create
-    @pic = Pic.new(pic_params)
+    @pic = current_user.photos.build(pic_params)
     if @pic.save
       redirect_to @pic
     else
