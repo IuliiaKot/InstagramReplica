89e73f44af90a34f27c3ecdd8e38c29ba2f23374
IuliiaKot <kotlenko.julia@gmail.com>
2016-08-22 22:34:51 -0400

    Paperclip gem and aploading image

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'paperclip', '~> 4.2.0'
+
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -48,6 +48,6 @@ class PicsController < ApplicationController
     end
 
     def pic_params
-      params.require(:pic).permit(:title, :description)
+      params.require(:pic).permit(:title, :description, :image)
     end
 end
